77be45a3056b96a33f9d79aac64fff5453591600
dave <david@omelett.es>
2014-06-24 17:52:25 +0100

    IDs for form inputs

--- module/OmelettesStub/src/OmelettesStub/Form/LoginForm.php
+++ module/OmelettesStub/src/OmelettesStub/Form/LoginForm.php
@@ -13,7 +13,7 @@ class LoginForm
     /**
      * @Form\Type("Zend\Form\Element\Email")
      * @Form\Options({"label":"Email Address"})
-     * @Form\Attributes({"placeholder":"Email Address"}) 
+     * @Form\Attributes({"id":"login-email"}) 
      */
     public $emailAddress;
     
@@ -21,7 +21,7 @@ class LoginForm
      * @Form\Type("Zend\Form\Element\Password")
      * @Form\Filter({"name":"StringTrim"})
      * @Form\Options({"label":"Password"})
-     * @Form\Attributes({"placeholder":"Password"}) 
+     * @Form\Attributes({"id":"login-password"})
      */
     public $password;
     
